47e7e257c81f3ccabce3ce97f16e1118eba10672
Jie Liang <jieliang456@gmail.com>
2018-02-14 19:01:13 -0500

    init backend with spring boot

---
+++ src/main/java/com/tabular/tabular/TabularApplication.java
@@ -0,0 +1,12 @@
+package com.tabular.tabular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabularApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tabular/tabular/TabularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tabular.tabular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TabularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
