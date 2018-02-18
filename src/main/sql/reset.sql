f38d4f6b07abec43fa4bcbc7a0b87cefd9bb663f
Jie Liang <jieliang456@gmail.com>
2018-02-18 13:44:36 -0500

    wrote sql script to drop all tables

---
+++ src/main/sql/reset.sql
@@ -0,0 +1,11 @@
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists appointment;
+drop table if exists customer;
+drop table if exists owner;
+drop table if exists restaurant;
+drop table if exists `table`;
+drop table if exists user;
+drop table if exists waiter;
+drop table if exists appointment2customer;
+drop table if exists appointment2waiter;
+SET FOREIGN_KEY_CHECKS = 1;
