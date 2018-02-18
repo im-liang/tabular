a99e3341263e1ba1f04b43393f226776a4c03ace
Jie Liang <jieliang456@gmail.com>
2018-02-18 17:02:19 -0500

    set current datetime as default time column in database

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -60,8 +60,8 @@ CREATE TABLE `waiter` (
 
 CREATE TABLE `appointment` (
   `appointment_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `start_time` DATETIME NOT NULL,
-  `status` varchar(30) NOT NULL,
+  `start_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int NOT NULL,
   `table_id` bigint(20) NOT NULL,
   PRIMARY KEY (`appointment_id`),
   FOREIGN KEY (`table_id`) REFERENCES `table` (table_id)
@@ -72,7 +72,7 @@ CREATE TABLE `appointment2customer` (
   `appointment2customer_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `appointment_id` bigint(20) NOT NULL,
   `customer_id` bigint(20) NOT NULL,
-  `time` DATETIME NOT NULL,
+  `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`appointment2customer_id`),
   FOREIGN KEY (`appointment_id`) REFERENCES appointment(`appointment_id`),
   FOREIGN KEY (`customer_id`) REFERENCES customer(`customer_id`)
@@ -83,7 +83,7 @@ CREATE TABLE `appointment2waiter` (
   `appointment2waiter_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `appointment_id` bigint(20) NOT NULL,
   `waiter_id` bigint(20) NOT NULL,
-  `time` DATETIME NOT NULL,
+  `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`appointment2waiter_id`),
   FOREIGN KEY (`appointment_id`) REFERENCES appointment(`appointment_id`),
   FOREIGN KEY (`waiter_id`) REFERENCES waiter(`waiter_id`)
