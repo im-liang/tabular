9ca334954f3191ff046ee77e46e98247cedf41f0
Jie Liang <jieliang456@gmail.com>
2018-02-18 15:57:10 -0500

    link spring config files at runtime

--- src/main/java/com/tabular/tabular/TabularApplication.java
+++ src/main/java/com/tabular/tabular/TabularApplication.java
@@ -1,9 +1,13 @@
 package com.tabular.tabular;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "config/spring/dao.xml", "config/spring/service.xml" })
+@MapperScan(basePackages = {"com.tabular.tabular.dao"})
 public class TabularApplication {
 
 	public static void main(String[] args) {
