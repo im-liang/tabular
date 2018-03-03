29f718f9d5d18907af597a26d0c9561e34c643c8
Jie Liang <jieliang456@gmail.com>
2018-03-03 10:14:55 -0500

    add method description to CustomerDao.java

--- src/main/java/com/tabular/tabular/dao/CustomerDao.java
+++ src/main/java/com/tabular/tabular/dao/CustomerDao.java
@@ -7,9 +7,44 @@ import java.util.List;
 import java.util.Map;
 
 public interface CustomerDao {
+    /**
+     * create a customer entry with its id matched to user id
+     *
+     * @param customerId
+     * @param name
+     * @return affected row
+     */
     int insertCustomer(@Param("customerId") long customerId, @Param("name") String name);
-    Customer queryCustomerById(long id);
+
+    /**
+     * find a customer with its id matched to the customerId
+     *
+     * @param customerId
+     * @return customer with matching id
+     */
+    Customer queryCustomerById(long customerId);
+
+    /**
+     * find a list of customers with its name matched to the name
+     *
+     * @param name
+     * @return list of customers with matching name
+     */
     List<Customer> queryCustomerByName(String name);
+
+    /**
+     * find all customers in the database
+     *
+     * @return list of customers
+     */
     List<Customer> queryAll();
+
+    /**
+     * modify the name of a customer entry
+     *
+     * @param customerId
+     * @param name
+     * @return affected row
+     */
     int modifyCustomerName(@Param("customerId") long customerId, @Param("name") String name);
 }
