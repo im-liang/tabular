94bb6c2d95e1c392cf115df4aab4070f37e91414
Jie Liang <jieliang456@gmail.com>
2018-03-02 22:44:36 -0500

    add method description to Appointment2UserDao.java

--- src/main/java/com/tabular/tabular/dao/Appointment2UserDao.java
+++ src/main/java/com/tabular/tabular/dao/Appointment2UserDao.java
@@ -6,10 +6,50 @@ import java.util.Date;
 import java.util.List;
 
 public interface Appointment2UserDao {
+    /**
+     * create a relation entry between appointment and user
+     *
+     * @param appointment2User
+     * @return affected row
+     */
     int createRelation(final Appointment2User appointment2User);
+
+    /**
+     * find list of relations based on appointment
+     *
+     * @param appointmentId
+     * @return list of relations
+     */
     List<Appointment2User> queryByAppointmentId(long appointmentId);
+
+    /**
+     * find list of relations based on user
+     *
+     * @param userId
+     * @return list of relations
+     */
     List<Appointment2User> queryByUserId(long userId);
+
+    /**
+     * find list of past relations
+     *
+     * @param datetime
+     * @return list of relations
+     */
     List<Appointment2User> queryPastRelations(Date datetime);
+
+    /**
+     * find all relations
+     *
+     * @return list of relations
+     */
     List<Appointment2User> queryAll();
+
+    /**
+     * delete relation using the id
+     *
+     * @param appointment2userId
+     * @return list of relations
+     */
     int deleteRelation(long appointment2userId);
 }
