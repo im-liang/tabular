5d0e9159f0d0068fb7a3667c0db64160ed99cfb6
Jie Liang <jieliang456@gmail.com>
2018-03-03 15:16:42 -0500

    implement user service to wrap methods in user dao and create unit tests

--- src/main/java/com/tabular/tabular/service/UserService.java
+++ src/main/java/com/tabular/tabular/service/UserService.java
@@ -1,7 +1,108 @@
 package com.tabular.tabular.service;
 
-import com.tabular.tabular.entity.Customer;
+import com.tabular.tabular.dao.UserDao;
+import com.tabular.tabular.entity.User;
+import com.tabular.tabular.enums.UserRoleEnum;
+import com.tabular.tabular.enums.UserStatusEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface CustomerService {
-    Customer getById(long customerId);
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+
+    public User queryUserById(long userId) {
+        return userDao.queryUserById(userId);
+    }
+
+    public User queryUserByUsername(String username) {
+        return userDao.queryUserByUsername(username);
+    }
+
+    int changeUsername(long userId, String username){
+        return userDao.modifyUsername(userId, username);
+    }
+
+    int changePassword(long userId, String password){
+        return userDao.modifyUserPassword(userId, password);
+    }
+
+    int deleteUserById(long userId){
+        return userDao.deleteUserById(userId);
+    }
+
+    int deleteUserByName(String username){
+        return userDao.deleteUserByName(username);
+    }
+
+
+    /**
+     * create a user which role is customer
+     *
+     * @param username
+     * @param password
+     * @return user id
+     */
+    public long createUserAsCustomer(String username, String password) {
+        User user = new User(username, password, UserRoleEnum.CUSTOMER.getRole(), UserStatusEnum.ACTIVE.getStatus());
+        userDao.createUser(user);
+        return user.getUserId();
+    }
+
+    /**
+     * create a user which role is waiter
+     *
+     * @param username
+     * @param password
+     * @return user id
+     */
+    public long createUserAsWaiter(String username, String password) {
+        User user = new User(username, password, UserRoleEnum.WAITER.getRole(), UserStatusEnum.ACTIVE.getStatus());
+        userDao.createUser(user);
+        return user.getUserId();
+    }
+
+    /**
+     * create a user which role is owner
+     *
+     * @param username
+     * @param password
+     * @return user id
+     */
+    public long createUserAsOwner(String username, String password) {
+        User user = new User(username, password, UserRoleEnum.OWNER.getRole(), UserStatusEnum.ACTIVE.getStatus());
+        userDao.createUser(user);
+        return user.getUserId();
+    }
+
+    /**
+     * create a relation entry between appointment and user
+     *
+     * @param userId
+     * @return boolean
+     */
+    boolean activateUser(long userId){
+        int affectedRow = userDao.modifyUserStatus(userId, UserStatusEnum.ACTIVE.getStatus());
+        if(affectedRow <= 0) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    /**
+     * create a relation entry between appointment and user
+     *
+     * @param userId
+     * @return affected row
+     */
+    boolean deActivateUser(long userId){
+        int affectedRow = userDao.modifyUserStatus(userId, UserStatusEnum.INACTIVE.getStatus());
+        if(affectedRow <= 0) {
+            return false;
+        }else {
+            return true;
+        }
+    }
 }
---
+++ src/test/java/com/tabular/tabular/service/UserServiceTest.java
@@ -0,0 +1,66 @@
+package com.tabular.tabular.service;
+
+import com.tabular.tabular.BaseTests;
+import com.tabular.tabular.entity.User;
+import com.tabular.tabular.enums.UserRoleEnum;
+import com.tabular.tabular.enums.UserStatusEnum;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+public class UserServiceTest extends BaseTests {
+    @Autowired
+    private UserService userService;
+
+    private long customerId, waiterId, ownerId;
+
+    private void checkUserEquality(User user, String username, String password, int role, int status) {
+        Assert.isTrue(user.getUsername().equals(username), "user username is not matched");
+        Assert.isTrue(user.getPassword().equals(password), "user password is not matched");
+        Assert.isTrue(user.getRole() == role, "user role is not matched");
+        Assert.isTrue(user.getStatus() == status, "user status is not matched");
+    }
+
+    @Before
+    public void initTest() {
+        customerId = userService.createUserAsCustomer("customer", "password+c");
+        waiterId = userService.createUserAsWaiter("waiter", "password+w");
+        ownerId = userService.createUserAsOwner("owner", "password+o");
+        userService.deActivateUser(waiterId);
+    }
+
+    @Test
+    public void testCreateUser() throws Exception {
+        User customer = userService.queryUserById(customerId);
+        checkUserEquality(customer, "customer", "password+c", UserRoleEnum.CUSTOMER.getRole(), UserStatusEnum.ACTIVE.getStatus());
+        User waiter = userService.queryUserById(waiterId);
+        checkUserEquality(waiter, "waiter", "password+w", UserRoleEnum.WAITER.getRole(), UserStatusEnum.INACTIVE.getStatus());
+        User owner = userService.queryUserById(ownerId);
+        checkUserEquality(owner, "owner", "password+o", UserRoleEnum.OWNER.getRole(), UserStatusEnum.ACTIVE.getStatus());
+    }
+
+    @Test
+    public void testDeactivateUser() throws Exception {
+        boolean result = userService.deActivateUser(customerId);
+        Assert.isTrue(result, "testDeactivateUser fail");
+        User user = userService.queryUserById(customerId);
+        Assert.isTrue(user.getStatus() == UserStatusEnum.INACTIVE.getStatus(), "testDeactivateUser fail");
+    }
+
+    @Test
+    public void testActivateUser() throws Exception {
+        boolean result = userService.activateUser(waiterId);
+        Assert.isTrue(result, "testDeactivateUser fail");
+        User user = userService.queryUserById(waiterId);
+        Assert.isTrue(user.getStatus() == UserStatusEnum.ACTIVE.getStatus(), "testActivateUser fail");
+    }
+
+    @After
+    public void cleanup() {
+        userService.deleteUserById(customerId);
+        userService.deleteUserById(waiterId);
+        userService.deleteUserById(ownerId);
+    }
+}
